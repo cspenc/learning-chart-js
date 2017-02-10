8387fc4a50cfb9663b41322ebfbb93fb8646ba7a
cspenc <crs992@gmail.com>
2017-02-09 17:51:38 -0700

    doin shapeways things

--- js/main.js
+++ js/main.js
@@ -1,33 +1,28 @@
-const CHART = document.getElementById("lineChart");
-console.log(CHART);
+const CHART = document.getElementById("consumerData");
 
-let lineChart = new Chart(CHART, {
-  type: 'line',
-  data: {
-    labels: ["January", "February", "March", "April", "May", "June", "July"],
+var data = {
+    labels: ["Bank of America", "Wells Fargo & Company", "Equifax", "Experian", "JPMorgan Chase & Co."],
     datasets: [
         {
-            label: "My First dataset",
-            fill: false,
-            lineTension: 0.1,
-            backgroundColor: "rgba(75,192,192,0.4)",
-            borderColor: "rgba(75,192,192,1)",
-            borderCapStyle: 'butt',
-            borderDash: [],
-            borderDashOffset: 0.0,
-            borderJoinStyle: 'miter',
-            pointBorderColor: "rgba(75,192,192,1)",
-            pointBackgroundColor: "#fff",
-            pointBorderWidth: 1,
-            pointHoverRadius: 5,
-            pointHoverBackgroundColor: "rgba(75,192,192,1)",
-            pointHoverBorderColor: "rgba(220,220,220,1)",
-            pointHoverBorderWidth: 2,
-            pointRadius: 1,
-            pointHitRadius: 10,
-            data: [65, 59, 80, 81, 56, 55, 40],
-            spanGaps: false,
-        }
-      ]
-    }
-})
+            data: [63428, 50958, 44411, 41816, 40551],
+            backgroundColor: [
+                "#B21212",
+                "#FFFC19",
+                "#FF0001",
+                "#1485CC",
+                "#0971B2"
+            ],
+            hoverBackgroundColor: [
+                "#B21212",
+                "#FFFC19",
+                "#FF0001",
+                "#1485CC",
+                "#0971B2"
+            ]
+        }]
+};
+
+var consumerData = new Chart(CHART, {
+    type: 'doughnut',
+    data: data
+});
